51829dfb0e808251a4cd87d04f7eb33a1cdd0f0b
RootUser_ <31113987+psalmy2595@users.noreply.github.com>
2022-08-02 14:21:28 +0100

    seed data working

--- app.js
+++ app.js
@@ -7,15 +7,16 @@ var express    = require("express"),
     // comment = require("./models/comment");
     // user = require("./models/user");
 
-seedDB();
+
 //APP CONFIG    
 const PORT = process.env.PORT || 7000;
 
 //create db and connect mongodb to app
-mongoose.connect('mongodb://localhost:27017/yelp_camp', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/yelp_campv3', {useNewUrlParser: true, useUnifiedTopology: true});
 app.use(express.urlencoded({ extended: true}));
 //to be able to call dir without the file eextension
 app.set("view engine", "ejs");
+seedDB();
 
 
 
--- seeds.js
+++ seeds.js
@@ -1,16 +1,46 @@
 var mongoose = require("mongoose");
 var Campground = require("./models/campground");
 
-
-function seedDB() {
-    //Delete All Campgrounds
-    Campground.deleteMany({}, function(err){
-        if (err){
-            consolle.log(err)
-        } else{
-            console.log("all campgrounds deleted")
+    //array of Objects
+    var data = [
+        {
+            name: "Cloud's Rest", 
+            image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
+            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+        },
+        {
+            name: "Cloud's Rest", 
+            image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
+            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+        },
+        {
+            name: "Canyon Floor", 
+            image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
+            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
         }
-    }); 
-}
+    ]
+
+    function seedDB() {
+            //Delete All Campgrounds
+            Campground.deleteMany({}, function(err){
+                if (err){
+                    consolle.log(err)
+                }
+                console.log("all campgrounds deleted")
+                    //add a few campgrounds
+                    data.forEach(function(seed){
+                        Campground.create(seed, function(err, campground){
+                            if(err){
+                                console.log(err)
+                            } else {
+                                console.log("added a campground");
+                                //create a comment   
+                            }
+                        });
+                });
+                
+            });  
+        
+    };
 
 module.exports = seedDB;
